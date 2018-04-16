e79682dc3956566fd1120731fe31e385d1c3815d
Nils Backe <nabacke@U16.04_AY17-18>
2018-04-16 17:02:37 -0400

    started csim.c

--- csim.c
+++ csim.c
@@ -1,7 +1,15 @@
 #include "cachelab.h"
 
+// Nils Backe nabacke
+// Varnika Sinha vsinha2
+// Team name: i_need_arrays
+
 int main()
 {
     printSummary(0, 0, 0);
     return 0;
+}
+
+void printSummary(int hits, int misses, int evictions) {
+	printf("hits:%d misses:%d evictions%d", hits, misses, evictions);
 }
