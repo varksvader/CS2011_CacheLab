4e3e74ec1b7c12da96806fd22809c0cc524b404a
Varnika Sinha <varnsinha@gmail.com>
2018-04-17 09:40:35 -0400

    Added prof's idea for cache from class

--- csim.c
+++ csim.c
@@ -4,6 +4,13 @@
 // Varnika Sinha vsinha2
 // Team name: i_need_arrays
 
+/* Ideas for cache:
+   - each line should be a struct
+   - each set should be an array of structs 
+   - cache should be an array of arrays of structs (2D array of structs)
+   - function to manipulate the cache (adding and flushing) 
+*/
+
 int main()
 {
     printSummary(0, 0, 0);
